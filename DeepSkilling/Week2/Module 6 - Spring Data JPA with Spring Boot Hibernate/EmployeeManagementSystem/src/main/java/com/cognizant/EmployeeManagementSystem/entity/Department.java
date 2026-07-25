8ec9f02dbc0c9c1c3535b9f158ffdda1ffa4867b
BitsofSubhashri <subhashridangua@gmail.com>
2026-07-25 21:42:18 +0530

    Initial commit

---
+++ DeepSkilling/Week2/Module 6 - Spring Data JPA with Spring Boot Hibernate/EmployeeManagementSystem/src/main/java/com/cognizant/EmployeeManagementSystem/entity/Department.java
@@ -0,0 +1,47 @@
+package com.cognizant.EmployeeManagementSystem.entity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @OneToMany(mappedBy = "department")
+    @JsonManagedReference
+    private List<Employee> employees;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
